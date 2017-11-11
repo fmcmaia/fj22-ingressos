da0a6e82332a5aa28b94d5b510e5d5138d4a77de
Felipe Maia <fcmaia@hotmail.com>
2017-11-11 13:11:06 -0200

    Exercicio 5.4

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -2,19 +2,37 @@ package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.*;
+
+
 import br.com.caelum.ingresso.model.descontos.Desconto;
 
+@Entity
 public class Ingresso {
 	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@ManyToOne
 	private Sessao sessao;
+	
+	@ManyToOne
+	private Lugar lugar;
+	
 	private BigDecimal preco;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoDeIngresso tipoDeIngresso;
 	
 	public Ingresso(){		
 	}
 	
-	public Ingresso(Sessao sessao, Desconto tipoDeDesconto){
+	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Lugar lugar){
 		this.sessao = sessao;
-		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+		this.tipoDeIngresso = tipoDeIngresso;
+		this.preco = this.tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+		this.lugar = lugar;
 	}
 	
 	public Sessao getSessao(){
@@ -23,6 +41,14 @@ public class Ingresso {
 	
 	public BigDecimal getPreco(){
 		return preco;
+	}
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
 	}
 
 }
